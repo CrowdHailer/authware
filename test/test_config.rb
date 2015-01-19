7ae2ab0554786777a54110847715bb2321393bf3
Peter Saxton <peterhsaxton@gmail.com>
2015-01-19 00:48:20 +0000

    transactional testing

--- test/test_config.rb
+++ test/test_config.rb
@@ -6,3 +6,15 @@ Bundler.require(:default, RACK_ENV)
 
 reporter_options = {color: true, slow_count: 5}
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+class MiniTest::Test
+  alias_method :_original_run, :run
+
+  def run(*args, &block)
+    result = nil
+    Sequel::Model.db.transaction(:rollback => :always, :auto_savepoint=>true) do
+      result = _original_run(*args, &block)
+    end
+    result
+  end
+end
